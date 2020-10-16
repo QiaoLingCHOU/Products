12efb55dc9a92e0a9f36c79e5f5fe9ced0e93ac1
Qiao-Ling CHOU <pamela81123@gmail.com>
2020-10-16 17:02:01 +0200

    Add fied name and fix the problem with the garbled when you write chinese using endcoding utf-8

--- products.py
+++ products.py
@@ -16,3 +16,9 @@ while True:
 print(products)
 for p in products:
 	print('The price of', p[0], 'is', p[1])
+
+#write to file
+with open('products.csv', 'w', encoding='utf-8') as f:
+	f.write('商品名稱,商品價錢\n')
+	for p in products:
+		f.write(p[0] + ',' + (p[1]) + '\n')
